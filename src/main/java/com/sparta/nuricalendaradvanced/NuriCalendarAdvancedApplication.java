41ebbb928232062904ce268897ecaaffba9ff4a4
nurigaon96 <nurigaon96@gmail.com>
2024-10-13 22:37:16 +0900

    init commit

---
+++ src/main/java/com/sparta/nuricalendaradvanced/NuriCalendarAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nuricalendaradvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriCalendarAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuriCalendarAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nuricalendaradvanced/NuriCalendarAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nuricalendaradvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriCalendarAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
